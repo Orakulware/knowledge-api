6b61af59323ba0b3e68e00b386f5fb42618973c3
InWamos <74014248+InWamos@users.noreply.github.com>
2026-07-03 21:51:43 +0000

    add: media table

--- src/knowledge_api/media_record/domain.py
+++ src/knowledge_api/media_record/domain.py
@@ -1,22 +1,58 @@
+import uuid
 from datetime import datetime
+from enum import StrEnum
 from typing import Any
 
 
+class MediaType(StrEnum):
+    TELEGRAM = "TELEGRAM"
+    NEWSPAPER = "NEWSPAPER"
+
+
+class Media:
+    """Model of media table.
+    Contains id key, media type and media name and reputation"""
+
+    id: uuid.UUID
+    media_type: MediaType
+    media_name: str
+    reputation: float
+
+
 class MediaRecord:
-    def __init__(
+    """Model of media_records table.
+    Contains the media records, including it's content,
+    a timestamp and contributor acknowledgements.
+    """
+
+    id: uuid.UUID
+    added_by: str
+    posted_in_media: uuid.UUID
+    content: str
+    posted_at: datetime
+    deleted_at: datetime | None
+    metadata: dict[str, Any]
+
+    def __init__(  # noqa: PLR0913
         self,
-        posted_in: str,
+        added_by: str,
+        posted_in_media: uuid.UUID,
         content: str,
         posted_at: datetime,
+        deleted_at: datetime | None = None,
         metadata: dict[str, Any] | None = None,
     ) -> None:
         if metadata is None:
             metadata = {}
-        self._posted_in = posted_in
-        self._content = content
-        self._posted_at = posted_at
-        self._metadata = metadata
+        # UUIDv7 best for creating an index.
+        self.id = uuid.uuid7()
+        self.added_by = added_by
+        self.posted_in_media = posted_in_media
+        self.content = content
+        self.posted_at = posted_at
+        self.deleted_at = deleted_at
+        self.metadata = metadata
 
     def __str__(self) -> str:
-        return f"{self._posted_in}[{self._posted_at}]: \
-            {self._content} \n Metadata: {self._metadata}"
+        return f"{self.added_by}[{self.posted_at}]: \
+            {self.content} \n Metadata: {self.metadata}"
