8174797d53a5b7722e36ef52c6fc955a4521935c
InWamos <74014248+InWamos@users.noreply.github.com>
2026-07-03 21:54:41 +0000

    add: argument to save media record method

--- src/knowledge_api/media_record/infrastructure/infrastructure.py
+++ src/knowledge_api/media_record/infrastructure/infrastructure.py
@@ -1,14 +1,18 @@
 from abc import ABC, abstractmethod
 
+from media_record.domain import MediaRecord
+from sqlalchemy.ext.asyncio import AsyncSession
+
 
 class MediaRecordRepository(ABC):
     @abstractmethod
-    async def save_media_record(self) -> None: ...
+    async def save_media_record(self, media_record: MediaRecord) -> None: ...
 
 
 class SQLAlchemyRecordRepository(MediaRecordRepository):
-    def __init__(self) -> None:
+    def __init__(self, session: AsyncSession) -> None:
         super().__init__()
+        self._session = session
 
-    async def save_media_record(self) -> None:
-        raise NotImplementedError
+    async def save_media_record(self, media_record: MediaRecord) -> None:
+        pass
