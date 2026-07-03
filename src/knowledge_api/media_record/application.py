72faf2c9ebbe3f4d7ff1a7a2b23de1dad12349d4
InWamos <74014248+InWamos@users.noreply.github.com>
2026-07-03 20:52:55 +0000

    add: request dataclass

--- src/knowledge_api/media_record/application.py
+++ src/knowledge_api/media_record/application.py
@@ -1,6 +1,21 @@
+import logging
+from dataclasses import dataclass
+
+from media_record import domain
+from shared.transaction_manager import TransactionManager
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True, slots=True)
+class PostMediaRecordRequest:
+    submitter_id: str
+    record: domain.MediaRecord
+
+
 class PostMediaRecord:
-    def __init__(self) -> None:
+    def __init__(self, transaction_manager: TransactionManager) -> None:
         pass
 
-    async def __call__(self) -> None:
+    async def __call__(self, request: PostMediaRecordRequest) -> None:
         raise NotImplementedError
