d96213a1c4c61d9d862d560d1734e6d448f4383b
InWamos <74014248+InWamos@users.noreply.github.com>
2026-07-03 21:06:10 +0000

    add: init method

--- src/knowledge_api/media_record/application.py
+++ src/knowledge_api/media_record/application.py
@@ -2,6 +2,8 @@ import logging
 from dataclasses import dataclass
 
 from media_record import domain
+from media_record.infrastructure.infrastructure import MediaRecordRepository
+from shared.caller_identity import CallerIdentity
 from shared.transaction_manager import TransactionManager
 
 logger = logging.getLogger(__name__)
@@ -9,13 +11,19 @@ logger = logging.getLogger(__name__)
 
 @dataclass(frozen=True, slots=True)
 class PostMediaRecordRequest:
-    submitter_id: str
     record: domain.MediaRecord
 
 
 class PostMediaRecord:
-    def __init__(self, transaction_manager: TransactionManager) -> None:
-        pass
+    def __init__(
+        self,
+        caller_identity: CallerIdentity,
+        transaction_manager: TransactionManager,
+        media_record_repository: MediaRecordRepository,
+    ) -> None:
+        self._caller_identity = caller_identity
+        self._transaction_manager = transaction_manager
+        self._media_record_repository = media_record_repository
 
     async def __call__(self, request: PostMediaRecordRequest) -> None:
         raise NotImplementedError
