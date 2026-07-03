383d7b25c3208249713c1a2558e2ca9de27bf54f
InWamos <74014248+InWamos@users.noreply.github.com>
2026-07-03 22:34:12 +0000

    add: media table

---
+++ src/knowledge_api/media_record/infrastructure/mappings/media.py
@@ -0,0 +1,31 @@
+from media_record.domain import Media, MediaType
+from media_record.infrastructure.mappings import registry
+from sqlalchemy import (
+    UUID,
+    Column,
+    Enum,
+    Float,
+    String,
+    Table,
+)
+
+media_table = Table(
+    "media",
+    registry.mapping_registry.metadata,
+    Column("id", UUID, primary_key=True),
+    Column("media_type", Enum(MediaType, create_constraint=True), nullable=False),
+    Column("media_name", String, nullable=False),
+    Column("reputation", Float, nullable=False),
+)
+
+
+def map_media_table() -> None:
+    registry.mapping_registry.map_imperatively(
+        Media,
+        media_table,
+        properties={
+            "media_type": media_table.c.media_type,
+            "media_name": media_table.c.media_name,
+            "reputation": media_table.c.reputation,
+        },
+    )
