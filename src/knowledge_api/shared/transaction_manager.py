9ac697a9358f3fda5272a24c6f029702ae6520a0
InWamos <74014248+InWamos@users.noreply.github.com>
2026-07-06 08:44:21 +0000

    add: sqla session transaction manager

--- src/knowledge_api/shared/transaction_manager.py
+++ src/knowledge_api/shared/transaction_manager.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
 
 class TransactionManager(ABC):
     @abstractmethod
@@ -9,3 +11,14 @@ class TransactionManager(ABC):
     @abstractmethod
     async def rollback(self) -> None:
         raise NotImplementedError
+
+
+class SQLAlchemySessionTransactionManager(TransactionManager):
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
+
+    async def commit(self) -> None:
+        await self._session.commit()
+
+    async def rollback(self) -> None:
+        await self._session.rollback()
