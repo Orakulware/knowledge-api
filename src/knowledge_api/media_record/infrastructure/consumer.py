a45cc03b3585c108569f59199902dae9150cb8fa
InWamos <74014248+InWamos@users.noreply.github.com>
2026-08-16 10:55:32 +0000

    refactor the base consumer method

--- src/knowledge_api/media_record/infrastructure/consumer.py
+++ src/knowledge_api/media_record/infrastructure/consumer.py
@@ -1,16 +1,51 @@
+import logging
+from abc import ABC, abstractmethod
+
 from config import RabbitMQConfig, RedisConfig
 from taskiq import AsyncBroker, AsyncResultBackend
 from taskiq_aio_pika import AioPikaBroker
 from taskiq_redis import RedisAsyncResultBackend
 
-result_backend: AsyncResultBackend = RedisAsyncResultBackend(
-    redis_url=RedisConfig.from_env().url,
-    keep_results=True,
-    result_ex_time=86400,
-)
+logger = logging.getLogger(__name__)
 
-broker: AsyncBroker = AioPikaBroker(
-    url=RabbitMQConfig.from_env().url,
-).with_result_backend(
-    result_backend,
-)
+
+class MediaRecordConsumer(ABC):
+    @abstractmethod
+    async def startup(self) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def shutdown(self) -> None:
+        raise NotImplementedError
+
+
+class TaskiqAioPikaRedisMediaRecordConsumer(MediaRecordConsumer):
+    """Media record consumer with Rabbit MQ broker and Redis backend"""
+
+    def __init__(
+        self,
+        rabbitmq_config: RabbitMQConfig,
+        redis_config: RedisConfig,
+    ) -> None:
+        self._rabbitmq_url = rabbitmq_config.url
+        self._redis_url = redis_config.url
+
+        self._result_backend: AsyncResultBackend = RedisAsyncResultBackend(
+            redis_url=self._redis_url,
+            keep_results=True,
+            result_ex_time=86400,
+        )
+
+        self._broker: AsyncBroker = AioPikaBroker(
+            url=self._rabbitmq_url,
+        ).with_result_backend(
+            self._result_backend,
+        )
+
+    async def startup(self) -> None:
+        logger.info("Starting up %s consumer", self.__class__.__name__)
+        await self._broker.startup()
+
+    async def shutdown(self) -> None:
+        logger.info("Shuting down %s consumer", self.__class__.__name__)
+        await self._broker.shutdown()
